701d10518845a08aaed7deb601b668d04621bdb7
mikomac405 <mikolajmacura0510@gmail.com>
2022-07-06 21:27:10 +0200

    Update mqtt_test_page.dart

--- lib/pages/mqtt_test_page.dart
+++ lib/pages/mqtt_test_page.dart
@@ -10,50 +10,66 @@ class MqttTestPage extends StatefulWidget {
 }
 
 class _MqttTestPageState extends State<MqttTestPage> {
-  double _value = 20;
+  final GlobalKey<FormState> _mqttRequestForm = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('FansPage'),
+        title: const Text('MQTT test page'),
       ),
       body: Center(
+          child: Form(
+        key: _mqttRequestForm,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: null,
-              child: const Text(
-                'On/Off',
-                style: TextStyle(fontSize: 72),
+            SizedBox(
+              width: 350,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Routing key',
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Routing key can\'t be null/empty!';
+                  }
+                  return null;
+                },
               ),
             ),
-            Column(
-              children: <Widget>[
-                Text(
-                  "Power",
-                  style: TextStyle(fontSize: 48),
+            SizedBox(
+              width: 350,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Message',
                 ),
-                Slider(
-                  min: 0.0,
-                  max: 100.0,
-                  value: _value,
-                  onChanged: (value) {
-                    setState(() {
-                      _value = value;
-                    });
+                validator: (String? value) {
+                  if (value == null) {
+                    return 'Message can\'t be null!';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            SizedBox(
+              width: 150,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_mqttRequestForm.currentState!.validate()) {
+                      // Send data
+
+                    }
                   },
+                  child: const Text("Send request"),
                 ),
-                Text(
-                  _value.toInt().toString() + "%",
-                  style: TextStyle(fontSize: 48),
-                ),
-              ],
+              ),
             )
           ],
         ),
-      ),
+      )),
     );
   }
 }
