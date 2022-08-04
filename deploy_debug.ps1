78d6b0b6da38c5c6c0d639bcfe26c1a98c7ee434
mikomac405 <mikolajmacura0510@gmail.com>
2022-08-04 15:16:30 +0200

    script

--- deploy_debug.ps1
+++ deploy_debug.ps1
@@ -1,18 +1,18 @@
 $lastGitHash = git log --pretty=format:"%h" -1
 Write-Output "Deploying Flutter Application to Raspberry Pi { commit hash: $lastGitHash }"
-if (Test-Connection -ComputerName raspberrypi.local -Count 1) {
+if (Test-Connection -ComputerName 10.180.73.17 -Count 1) {
     Write-Output "Connection to raspberrpi established."
 
     flutter build bundle
     Write-Output "Builded flutter bundle."
 
-    ssh build@raspberrypi "mkdir /home/build/flutter_app_$lastGitHash"
+    ssh pi@raspberrypi "mkdir /home/pi/flutter_app_$lastGitHash"
     Write-Output "Created directory on remote for the latest version of app."
 
-    scp -r ./build/flutter_assets/ build@raspberrypi:/home/build/flutter_app_$lastGitHash
+    scp -r ./build/flutter_assets/ pi@raspberrypi:/home/pi/flutter_app_$lastGitHash
     Write-Output "Copied application assets to raspberry."
 
-    ssh build@raspberrypi "flutter-pi /home/build/flutter_app_$lastGitHash/flutter_assets"
+    ssh pi@raspberrypi "flutter-pi /home/pi/flutter_app_$lastGitHash/flutter_assets"
     Write-Output "Tried to launch app on raspberry."
 } else {
     Write-Output "Connection failed."
