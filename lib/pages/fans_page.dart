4e0d97c15bf114944ba8740f5cd36664abc9dcf5
AlexKulpa <82471798+AlexKulpa@users.noreply.github.com>
2022-10-17 00:54:38 +0200

    added number pickers and fan UI major change

--- lib/pages/fans_page.dart
+++ lib/pages/fans_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inzynierka/globals.dart';
-import 'package:number_inc_dec/number_inc_dec.dart';
+import 'package:numberpicker/numberpicker.dart';
 
 class FansPage extends StatefulWidget {
   const FansPage({Key? key, required this.title}) : super(key: key);
@@ -12,7 +12,9 @@ class FansPage extends StatefulWidget {
 }
 
 class _FansPageState extends State<FansPage> {
-  double _value = 20;
+  int _temperatureValue = 25;
+  int _humidityValue = 50;
+  double _fanPowerValue = 20;
 
   @override
   Widget build(BuildContext context) {
@@ -24,62 +26,98 @@ class _FansPageState extends State<FansPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () =>
-                  connection.setConfig('vent', 'on', _value.toString()),
-              child: const Text(
-                'On',
-                style: TextStyle(fontSize: 72),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () =>
-                  connection.setConfig('vent', 'off', _value.toString()),
-              child: const Text(
-                'Off',
-                style: TextStyle(fontSize: 72),
-              ),
-            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ElevatedButton(
+                    onPressed: () => connection.setConfig(
+                        'vent', 'on', _fanPowerValue.toString()),
+                    child: const Text(
+                      'On',
+                      style: TextStyle(fontSize: 72),
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: () => connection.setConfig(
+                        'vent', 'off', _fanPowerValue.toString()),
+                    child: const Text(
+                      'Off',
+                      style: TextStyle(fontSize: 72),
+                    ),
+                  ),
+                ]),
             Column(
               children: <Widget>[
-                const Text(
-                  "Power",
-                  style: TextStyle(fontSize: 48),
-                ),
-                Slider(
-                  min: 0.0,
-                  max: 100.0,
-                  value: _value,
-                  onChanged: (value) {
-                    setState(() {
-                      _value = value;
-                    });
-                  },
-                ),
-                Text(
-                  _value.toInt().toString() + "%",
-                  style: const TextStyle(fontSize: 48),
-                ),
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "Power",
+                        style: TextStyle(fontSize: 30),
+                      ),
+                      SizedBox(
+                          width: 500,
+                          child: (Slider(
+                            min: 0.0,
+                            max: 100.0,
+                            thumbColor: Colors.blueGrey,
+                            value: _fanPowerValue,
+                            onChanged: (value) {
+                              setState(() {
+                                _fanPowerValue = value;
+                              });
+                            },
+                          ))),
+                      Text(
+                        _fanPowerValue.toInt().toString() + "%",
+                        style: const TextStyle(fontSize: 30),
+                      ),
+                    ])
               ],
             ),
-            NumberInputWithIncrementDecrement(
-              controller: TextEditingController(),
-              min: 0,
-              max: 100,
-            ),
-            const Text(
-              "Wilgotność",
-              style: TextStyle(fontSize: 48),
-            ),
-            NumberInputWithIncrementDecrement(
-              controller: TextEditingController(),
-              min: 0,
-              max: 100,
-            ),
-            const Text(
-              "Temperatura",
-              style: TextStyle(fontSize: 48),
-            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "Humidity",
+                    style: TextStyle(fontSize: 30, height: 3.5),
+                  ),
+                  SizedBox(
+                      child: FittedBox(
+                          fit: BoxFit.contain,
+                          child: NumberPicker(
+                            value: _humidityValue,
+                            minValue: 0,
+                            maxValue: 100,
+                            onChanged: (value) =>
+                                setState(() => _humidityValue = value),
+                          ))),
+                  const Text(
+                    "%",
+                    style: TextStyle(fontSize: 30, height: 3.5),
+                  ),
+                  const SizedBox(
+                    width: 100,
+                  ),
+                  const Text(
+                    "Temperature",
+                    style: TextStyle(fontSize: 30, height: 3.5),
+                  ),
+                  NumberPicker(
+                    value: _temperatureValue,
+                    minValue: 0,
+                    maxValue: 100,
+                    onChanged: (value) =>
+                        setState(() => _temperatureValue = value),
+                  ),
+                  const Text(
+                    "°C",
+                    style: TextStyle(fontSize: 30, height: 3.5),
+                  )
+                ])
           ],
         ),
       ),
